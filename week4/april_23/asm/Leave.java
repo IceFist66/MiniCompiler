b0bd8fef3c4f04ac40d0b21ba3f420b9629d7a84
IceFist66 <icefist66@gmail.com>
2014-05-21 17:22:37 -0700

    Leave created

---
+++ week4/april_23/asm/Leave.java
@@ -0,0 +1,12 @@
+package asm;
+
+public class Leave extends Instruction_a{
+
+	public Leave(){
+		this.arg1 = null;
+		this.arg2 = null;
+		this.arg3 = null;
+		this.text = "leave " + null;
+	}
+
+}
