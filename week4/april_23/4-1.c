6cd13fff71bcb269bdf211c4d2f3bf48c50ce11d
IceFist66 <icefist66@gmail.com>
2014-05-21 17:39:24 -0700

    new files

---
+++ week4/april_23/4-1.c
@@ -0,0 +1,13 @@
+int main(int ke)
+{
+   int i, j, k;
+   int b;
+   int h;
+   {
+      b = 1;
+      i = 3;
+      j = 4;
+      k = i + j + 3;
+   }
+   return 0;
+}
---
+++ week4/april_23/4-1.s
@@ -0,0 +1,30 @@
+	.file	"4-1.c"
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -36(%rbp)
+	movl	$1, -8(%rbp)
+	movl	$3, -20(%rbp)
+	movl	$4, -16(%rbp)
+	movl	-16(%rbp), %eax
+	movl	-20(%rbp), %edx
+	leal	(%rdx,%rax), %eax
+	addl	$3, %eax
+	movl	%eax, -12(%rbp)
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-3)"
+	.section	.note.GNU-stack,"",@progbits
