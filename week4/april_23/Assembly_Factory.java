0e15feadd775db3f01829ec1b1a16b0451743281
IceFist66 <icefist66@gmail.com>
2014-05-28 10:09:55 -0700

    new java file Assembly_Factory

---
+++ week4/april_23/Assembly_Factory.java
@@ -0,0 +1,78 @@
+import iloc.*;
+import asm.*;
+
+import java.util.ArrayList;
+
+public class Assembly_Factory {
+	private ArrayList<Node> input;
+	
+	public Assembly_Factory(ArrayList<Node> input){
+		this.input = input;
+	}
+
+	public ArrayList<Node> getInput() {
+		return input;
+	}
+
+	public void setInput(ArrayList<Node> input) {
+		this.input = input;
+	}
+	
+	public void convert(){
+		ArrayList<Instruction_a> asm;
+		for(Node n: input){
+			ArrayList<Instruction> instructions = n.getInstructions();
+			for(Instruction inst : instructions){
+				asm = getAssembly(inst);
+				n.getAsmInstructions().addAll(asm);
+			}
+			successors(n);
+            n.printAsm(); //prints the asm instructions to screen
+	    }
+	}
+
+    public void successors(Node n) {
+      ArrayList<Instruction> instructions;
+      ArrayList<Instruction_a> asm;
+      for (Node s : n.getSuccNodes()) {
+         instructions = s.getInstructions();
+         for(Instruction inst : instructions){
+        	 asm = getAssembly(inst);
+        	 s.getAsmInstructions().addAll(asm);
+         }
+         successors(s);
+      }    
+      return;
+    }
+    
+    public ArrayList<Instruction_a> getAssembly(Instruction i){
+    	ArrayList<Instruction_a> list = new ArrayList<Instruction_a>();
+        String arg1 = i.getArg1(), arg2 = i.getArg2(), arg3 = i.getArg3();
+    	if(i instanceof Mov){
+    		list = getMovq(arg1, arg2);
+    	}
+        else if(i instanceof Add){
+            list = getAddq(arg1, arg2, arg3);
+        }
+        else if(i instanceof Addi){
+            list = getAddq(arg1, "$"+arg2, arg3); //$ needed for numbers
+        }//whatever else
+    	else{
+            list = getMovq("$0", "$0");
+    	}
+    	return list;
+    }
+
+    public Movq getMovq(String arg1, String arg2){
+        ArrayList<Instruction_a> list = new ArrayList<Instruction_a>();
+        list.add(new Movq(arg1, arg2)); //r1 -> r2
+        return list;
+    }
+
+    public ArrayList<Instruction_a> getAddq(String arg1, String arg2, String arg3){
+        ArrayList<Instruction_a> list = new ArrayList<Instruction_a>();
+        list.addAll(getMovq(arg2, arg3)); //r3 = r2
+        list.add(new Addq(arg1, arg3)); //r3 += r1
+        return list;
+    }
+}
