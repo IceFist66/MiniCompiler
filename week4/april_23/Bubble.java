d63d831156956ff282a9d2d134d6b168799a410f
Kevin Elliott <kvnelliott@hotmail.com>
2014-06-04 00:18:41 -0700

    Finished adding coloring functionality to IGraph.java

--- week4/april_23/Bubble.java
+++ week4/april_23/Bubble.java
@@ -1,15 +1,17 @@
 import java.util.ArrayList;
 
 
-public class Bubble {
+public class Bubble implements Comparable<Bubble> {
 	private String id;
-	private int color;
+	private Color color;
+	private boolean constrained;
 	private ArrayList<Bubble> edges;
 	
 	
 	public Bubble(String id) {
 		this.id = id;
-		this.color = -1;
+		this.color = Color.UNC;
+		this.constrained = false;
 		this.edges = new ArrayList<Bubble>();
 	}
 	
@@ -19,17 +21,27 @@ public class Bubble {
 	public void setId(String id) {
 		this.id = id;
 	}
-	public int getColor() {
+	public Color getColor() {
 		return color;
 	}
-	public void setColor(int color) {
+	public void setColor(Color color) {
 		this.color = color;
+	}
+	public void setConstrained(boolean constrained) {
+	   this.constrained = constrained;
+	}
+	public boolean isConstrained() {
+	   return this.constrained;
 	}
 	public ArrayList<Bubble> getEdges() {
 		return edges;
 	}
 	public void setEdges(ArrayList<Bubble> edges) {
 		this.edges = edges;
+	}
+	public int compareTo(Bubble compareBubble) {
+	   int compareDegree = ((Bubble) compareBubble).edges.size();
+	   return compareDegree - this.edges.size();
 	}
 }
 
--- week4/april_23/IGraph.java
+++ week4/april_23/IGraph.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Stack;
 
 import iloc.*;
 import asm.*;
@@ -7,12 +9,12 @@ import asm.*;
 
 public class IGraph {
 	private ArrayList<Bubble> bubbles;
-	private ArrayList<Integer> colors;
+	private ArrayList<Color> colors;
 	private ArrayList<Node> nodes;
 	
 	public IGraph(ArrayList<Node> nodes) {
 		this.bubbles = new ArrayList<Bubble>();
-		this.colors = new ArrayList<Integer>();
+		this.colors = createColorSet();
 		this.nodes = nodes;
         createGraph();
 	}
@@ -23,11 +25,17 @@ public class IGraph {
 	public void setBubbles(ArrayList<Bubble> bubbles) {
 		this.bubbles = bubbles;
 	}
-	public ArrayList<Integer> getColors() {
+	public ArrayList<Color> getColors() {
 		return colors;
 	}
-	public void setColors(ArrayList<Integer> colors) {
+	public void setColors(ArrayList<Color> colors) {
 		this.colors = colors;
+	}
+	
+	public ArrayList<Color> createColorSet() {
+	   ArrayList<Color> col = new ArrayList<Color>(EnumSet.allOf(Color.class));
+	   col.remove(Color.UNC);
+	   return col;
 	}
 	
 	public void createGraph(){
@@ -35,6 +43,8 @@ public class IGraph {
 		ArrayList<String> liveNow = new ArrayList<String>();
 		String target;
 		ArrayList<String> sources = new ArrayList<String>();
+		int numColors = colors.size();
+		
 		for(Node n : nodes){
 			reverse.addAll(n.getAsmInstructions());
 			Collections.reverse(reverse);
@@ -55,10 +65,18 @@ public class IGraph {
 					    for(String s : liveNow){
 						    Bubble bsource = getBubble(s);
 						    if(bsource != null){
-							    if(!btarget.getEdges().contains(bsource))
+							    if(!btarget.getEdges().contains(bsource)) {
 								    btarget.getEdges().add(bsource);
-							    if(!bsource.getEdges().contains(btarget))
+								    if (btarget.getEdges().size() >= numColors)
+								      if (btarget.isConstrained() == false)
+								         btarget.setConstrained(true);
+								 }
+							    if(!bsource.getEdges().contains(btarget)) {
 								    bsource.getEdges().add(btarget);
+								    if (bsource.getEdges().size() >= numColors)
+								      if (bsource.isConstrained() == false)
+								         bsource.setConstrained(true);
+								 }
 						    }
 						    else{
 							    System.out.println("BSource is null");
@@ -92,6 +110,83 @@ public class IGraph {
 			}
 		}
 		return null;
+	}
+	
+	public void colorGraph() {
+	
+	   // still need to go through list of bubbles and assign colors to predefined registers
+	   // then determine when they will be added
+	
+	   int numColors = colors.size();
+	   Stack<Bubble> stackOfBubbles = new Stack<Bubble>();
+	   
+	      // push the unconstrained bubbles first
+	      for (Bubble b : bubbles) {
+	         if (b.isConstrained() == false) {
+	            bubbles.remove(b);
+               stackOfBubbles.push(b);            
+            }
+	      }
+	      
+	      // now sort the constrained bubbles in descending order by degree
+	      Collections.sort(bubbles);
+	      
+	      // push the now ordered set of constrained bubbles
+	      for (Bubble c : bubbles) {
+            bubbles.remove(c);
+            stackOfBubbles.push(c);            
+	      }
+	      
+	      // now add the bubbles back to the graph and try to color
+	      // assign Color.UNC to bubbles that can't be colored
+	      boolean done, conflict;
+	      int count;
+	      
+	      while (!stackOfBubbles.empty()) {
+	         done = false;        // attempted coloring of one bubble
+	         conflict = false;    // conflict for a particular color for a particular bubble
+	         count = 0;           // color number
+	         Bubble b = stackOfBubbles.pop();
+	         Color color;
+	         while (done == false && count < numColors) {
+	            color = colors.get(count++);
+	            
+	            // compare current color with each of the bubble's neighbors
+	            // and set conflict to true if there is a clash
+	            for (Bubble n : b.getEdges()) {
+                  if (bubbles.contains(n) && n.getColor() == color)
+                     conflict = true;   	
+               }
+               
+               // if no conflict was found, apply the color to the bubble and
+               // flag the coloring for this bubble as done
+               if (conflict == false) {
+                  b.setColor(color);
+                  done = true;
+               }
+               // if a conflict was found, reset the flag to check for the next color 
+               else {
+                  conflict = false;
+               }
+	         }
+	         // if after trying all colors there is still a conflict
+	         // then color the bubble as uncolorable
+	         if (conflict == true)
+	            b.setColor(Color.UNC);
+	         // the bubble is colored and can now be added back to the graph
+	         bubbles.add(b);
+	      }	      
+	      
+	}
+	
+	public void printBubbleColors() {
+	   for (Bubble b : bubbles) {
+	      System.out.println("Bubble " + b.getId() + " (" + b.getColor().text() + ")");
+	      for(Bubble n : b.getEdges()){
+				System.out.print(n.getId() + " (" + n.getColor().text() + ")");
+			}
+			System.out.println();
+	   }
 	}
 	
 	public void printBubbles(){
