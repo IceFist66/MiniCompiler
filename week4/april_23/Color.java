ea1178264887dfbda8eff22d295678779141007e
Kevin Elliott <kvnelliott@hotmail.com>
2014-06-03 21:57:23 -0700

    added Color.java for IGraph

---
+++ week4/april_23/Color.java
@@ -0,0 +1,17 @@
+// see notes 4-16
+
+public enum Color {
+	RAX("%rax"), RBX("%rbx"), RCX("%rcx"), RDX("%rdx"), RSP("%rsp"), RBP("%rbp"),
+	   RSI("%rsi"), RDI("%rdi"), R8("%r8"), R9("%r9"), R10("%r10"), 
+	   R11("%r11"), R12("%r12"), R13("%r13"), R14("%r14"), R15("%r15");
+	   
+	private final String text;
+	
+	private Color(final String text) {
+	   this.text = text;
+	}
+	
+	public String text() {
+	   return this.text;
+	}
+}
