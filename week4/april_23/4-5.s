8f96bcbd087cc19e51baa64d7409b89169cdcb2c
IceFist66 <icefist66@gmail.com>
2014-06-06 21:33:25 -0700

    new 4-5 files

---
+++ week4/april_23/4-5.s
@@ -0,0 +1,75 @@
+	.file	"4-5.c"
+	.comm	i,4,4
+	.comm	j,4,4
+	.section	.rodata
+.LC0:
+	.string	"%d"
+.LC1:
+	.string	"Hello"
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	$0, i(%rip)
+	movl	i(%rip), %edx
+	movl	$.LC0, %eax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf
+	movl	$.LC1, %edi
+	call	puts
+	movl	i(%rip), %eax
+	movl	%eax, %edi
+	call	simple
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.rodata
+.LC2:
+	.string	"%d\n"
+	.text
+.globl simple
+	.type	simple, @function
+simple:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movl	$3, j(%rip)
+	movl	i(%rip), %edx
+	movl	j(%rip), %eax
+	leal	(%rdx,%rax), %eax
+	movl	%eax, -4(%rbp)
+	movl	$.LC2, %eax
+	movl	-4(%rbp), %edx
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	simple, .-simple
+	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-3)"
+	.section	.note.GNU-stack,"",@progbits
