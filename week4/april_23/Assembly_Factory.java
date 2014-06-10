18e22da4b73e1e07c7e264441385f80a65bf0f9d
IceFist66 <icefist66@gmail.com>
2014-06-10 11:45:57 -0700

    fixed <, <=, ==

--- week4/april_23/Assembly_Factory.java
+++ week4/april_23/Assembly_Factory.java
@@ -175,7 +175,7 @@ public class Assembly_Factory {
             list = getOrq(arg1, arg2, arg3);
         }
         else if(i instanceof Comp){
-            list = getCmp(arg1, arg2);
+            list = getCmpq(arg1, arg2);
         }
         else if(i instanceof Cbreq){
             list = getJe(arg2, arg3);
@@ -387,10 +387,10 @@ public class Assembly_Factory {
         return list;
     }
 
-    public ArrayList<Instruction_a> getCmp(String arg1, String arg2){
+    public ArrayList<Instruction_a> getCmpq(String arg1, String arg2){
         ArrayList<Instruction_a> list = new ArrayList<Instruction_a>();
-        list.add(new Cmp(arg1, arg2)); //original code
-        //list.add(new Cmp(arg2, arg1));
+        list.add(new Cmpq(arg1, arg2)); //original code
+        //list.add(new Cmpq(arg2, arg1));
         return list;
     }
 
@@ -473,8 +473,8 @@ public class Assembly_Factory {
         ArrayList<Instruction_a> list = new ArrayList<Instruction_a>();
         list.add(new Pushq("%r15"));
         list.add(new Movq("$1", "%r15"));
-        list.add(new Cmovlq("%r15", arg2)); //Flipped for testing purposes PROBLEM UNSOLVED
-        //list.add(new Cmovgq("%r15", arg2));
+        //list.add(new Cmovlq("%r15", arg2)); //Flipped for testing purposes PROBLEM UNSOLVED
+        list.add(new Cmovgq("%r15", arg2));
         list.add(new Popq("%r15"));
         return list;
     }
@@ -483,7 +483,8 @@ public class Assembly_Factory {
         ArrayList<Instruction_a> list = new ArrayList<Instruction_a>();
         list.add(new Pushq("%r15"));
         list.add(new Movq("$1", "%r15"));
-        list.add(new Cmovleq("%r15", arg2));
+        //list.add(new Cmovleq("%r15", arg2)); //original code
+        list.add(new Cmovgeq("%r15", arg2));
         list.add(new Popq("%r15"));
         return list;
     }
@@ -500,9 +501,9 @@ public class Assembly_Factory {
     public ArrayList<Instruction_a> getBrz(String arg1, String arg2, String arg3){
         ArrayList<Instruction_a> list = new ArrayList<Instruction_a>();
         list.add(new Pushq("%r15"));
-        list.add(new Movq("$0", "%r15"));
-        list.add(new Cmp("%r15", arg1)); //original code
-        //list.add(new Cmp(arg1, "%r15"));
+        list.add(new Movq("$1", "%r15"));
+        list.add(new Cmpq("%r15", arg1)); //original code
+        //list.add(new Cmpq(arg1, "%r15"));
         list.add(new Popq("%r15"));
         //list.addAll(getJne(arg2, arg3)); //original code NOT SURE due to Jeff
         list.addAll(getJe(arg2, arg3));
